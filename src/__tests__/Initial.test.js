00d523b610ceeed13b81432748ce6726298b5c45
Vladyslav Kyiashko <kiyashko.vlad1@gmail.com>
2021-12-27 19:18:40 +0100

    Fix Test GitHub action error

---
+++ src/__tests__/Initial.test.js
@@ -0,0 +1,3 @@
+it('test example', () => {
+  expect(2).toBe(2);
+});
---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,7 @@
+interface Props {}
+
+const Home = (props: Props): JSX.Element => {
+  return <h1>Home Page</h1>;
+};
+
+export default Home;
