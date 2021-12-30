37a50a009a6ef4f6ac9021b2174aa789830d9dd4
Vladyslav Kyiashko <kiyashko.vlad1@gmail.com>
2021-12-30 22:43:28 +0100

    Created ScoreContext to pass score between routes.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useRoutes } from 'react-router-dom';
 import { NickNameContext } from './contexts/NickNameContext';
 import { routes } from './Routes';
+import { ScoreContext } from './contexts/ScoreContext';
 import './styles/main.scss';
 
 function App() {
@@ -10,11 +11,14 @@ function App() {
   // However I prefered to add it, because our RouteElement could change in the future
   // and we wouldn't need to use props drilling
   const [globalNickName, setGlobalNickName] = useState('');
+  const [globalScore, setGlobalScore] = useState(NaN);
 
   return (
     <div className="view-layout view-layout--center">
       <NickNameContext.Provider value={{ globalNickName, setGlobalNickName }}>
-        {RouteElement}
+        <ScoreContext.Provider value={{ globalScore, setGlobalScore }}>
+          {RouteElement}
+        </ScoreContext.Provider>
       </NickNameContext.Provider>
     </div>
   );
